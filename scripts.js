29d4bf3cc69e1c92f35d26967a0cdedf71f4dafe
APWaldo <apw2131@columbia.edu>
2014-02-21 16:06:10 -0500

    Adding function teaDiv to the scripts.

--- scripts.js
+++ scripts.js
@@ -2,8 +2,22 @@
  * @author Annie P Waldman
  */
 
+//loading function teaDiv
+function teaDiv(){
+	//alerting me that I got to document ready and the function was loaded at document ready.
+	console.log("I got to document ready!");
 
+	var myDiv = $("<div>");
+	//creating a new div.
+	$(myDiv).html("This is my new div");
+	//putting some code in my div saying this is my new div
+	//adding my div to tea container in my html
+	$("#teaContainer").append(myDiv);
 
+	//loading json tea_types
+	$.get("tea_types.json", loadedJSON, "json");
+
+}
 
 
 
