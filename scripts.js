6808dfdecfedbe3984d8a6fc770ef6a591b946ea
APWaldo <apw2131@columbia.edu>
2014-02-21 16:46:05 -0500

    Adding fuction addTeaDatatoPage to scripts and pulling json data to page, and console logging it.

--- scripts.js
+++ scripts.js
@@ -2,24 +2,36 @@
  * @author Annie P Waldman
  */
 
+function addTeaDatatoPage(TeaData){
+	//console log my tea data.
+	console.log (TeaData);
+	var myTeaDataDiv = $("<div>");
+	//add first tea name of tea data to div.
+	$(myTeaDataDiv).html(TeaData.myDreamTeas[0].name);
+	//console logging (not putting in div) one of the tea names.
+	console.log (TeaData.myDreamTeas[1].name);
+	//add new tea data to page.
+	$("#teaContainer").append(myTeaDataDiv);
+}
+
 //loading function teaDiv
 function teaDiv(){
 	//alerting me that I got to document ready and the function was loaded at document ready.
 	console.log("I got to document ready!");
-
-	var myDiv = $("<div>");
 	//creating a new div.
-	$(myDiv).html("This is my new div");
+	var myDiv = $("<div>");
 	//putting some code in my div saying this is my new div
+	$(myDiv).html("Adding a new div to the page.");
 	//adding my div to tea container in my html
 	$("#teaContainer").append(myDiv);
-
 	//loading json tea_types
-	$.get("tea_types.json", loadedJSON, "json");
+	$.get("tea_types.json", addTeaDatatoPage, "json");
 
 }
 
 
-
 //Adding document ready and also telling it to load teaDiv function.
 $( document ).ready(teaDiv);
+
+
+console.log("This statement is after document ready.");
